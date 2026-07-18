33d5b85ccbe1a05b3ae26d48d93252012546933b
Christian Hüning <christianhuening@posteo.de>
2026-07-18 21:20:49 +0200

    add handling of backupentry to fix #29

--- pkg/controller/backupentry/add.go
+++ pkg/controller/backupentry/add.go
@@ -10,6 +10,7 @@ import (
 
 	"github.com/gardener/gardener/extensions/pkg/controller/backupentry"
 	"github.com/gardener/gardener/extensions/pkg/controller/backupentry/genericactuator"
+	extensionsv1alpha1 "github.com/gardener/gardener/pkg/apis/extensions/v1alpha1"
 	"sigs.k8s.io/controller-runtime/pkg/controller"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
 
@@ -27,6 +28,8 @@ type AddOptions struct {
 	Controller controller.Options
 	// IgnoreOperationAnnotation specifies whether to ignore the operation annotation or not.
 	IgnoreOperationAnnotation bool
+	// ExtensionClass defines the extension class this extension is responsible for.
+	ExtensionClass extensionsv1alpha1.ExtensionClass
 }
 
 // AddToManagerWithOptions adds a controller with the given Options to the given manager.
@@ -37,6 +40,7 @@ func AddToManagerWithOptions(_ context.Context, mgr manager.Manager, opts AddOpt
 		ControllerOptions: opts.Controller,
 		Predicates:        backupentry.DefaultPredicates(opts.IgnoreOperationAnnotation),
 		Type:              s3.Type,
+		ExtensionClass:    opts.ExtensionClass,
 	})
 }
 
